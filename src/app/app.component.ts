dfbf95f631a17735fb472095eaab99f741ad65f5
AgeevDmitryMinsk <ageev.dmitry@outlook.com>
2023-01-08 10:29:33 +0000

    some problem with undefined

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {API_KEY, DataService} from "./components/services/data.service";
+
+export interface IGenre {
+  id: number,
+  name: string
+}
+
+export interface IGenres {
+  genres: IGenre[]
+
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'CinemaAngular2023TMDB';
+export class AppComponent implements OnInit {
+
+  movies: any[] = [];
+
+  allGenres$: Observable<IGenres>
+
+  genres: IGenre[]
+  genresTV: IGenre[]
+
+  clickedGenre: string = ''
+  clickedGenreMovie_TV: string = ''
+  clickedGenreID?: number
+
+  movie_tv: string
+  request = {
+    // fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28&language=ru-RU`,
+    fetchActionMovies: `/discover/${this.clickedGenreMovie_TV}?api_key=${API_KEY}&with_genres=${this.clickedGenreID}&language=ru-RU`,
+  }
+  constructor(
+    private http: HttpClient,
+    private dataService: DataService
+  ) {
+  }
+
+
+  ngOnInit() {
+    this.dataService.getGenresMovieData().subscribe((result) => {
+      console.log(JSON.stringify(result))
+      this.genres = result.genres
+    })
+
+    this.dataService.getGenresTV_Data().subscribe(result => {
+      this.genresTV = result.genres
+    })
+  }
+
+  clickedHandle(event_genre: string, event_genre_id: number, movie_tv: string) {
+    console.log("click =", event_genre, ',', movie_tv, ',', event_genre_id)
+    this.clickedGenre = event_genre
+    this.clickedGenreID = event_genre_id
+    this.clickedGenreMovie_TV = movie_tv
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+	imports: [
+		BrowserModule,
+		HttpClientModule,
+		FormsModule
+	],
   providers: [],
   bootstrap: [AppComponent]
 })
