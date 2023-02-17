d07fca1c8b4935497a526b8a99c6fe6df292d3d5
AgeevDmitryMinsk <ageev.dmitry@outlook.com>
2023-02-17 14:57:46 +0000

    add checking for some error in HttpInterceptor

--- src/app/interceptors/param.interceptor.ts
+++ src/app/interceptors/param.interceptor.ts
@@ -3,15 +3,17 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor, HttpErrorResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {catchError, Observable, throwError} from 'rxjs';
 import {environment} from "../../environments/environment";
+import {ToastrService} from "ngx-toastr";
+
 
 @Injectable()
 export class ParamInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(private toastr: ToastrService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
 
@@ -23,9 +25,47 @@ export class ParamInterceptor implements HttpInterceptor {
           environment.API_KEY
         )
       });
-      return next.handle(paramReq);
+      return next.handle(paramReq)
+        .pipe(
+          catchError((error: HttpErrorResponse) => {
+            let errorMsg = '';
+            if (error.error instanceof ErrorEvent) {
+              console.log('1 This is client side error FROM HttpInterceptor');
+              errorMsg = `Error: ${error.error.message}`;
+            } else {
+              console.log('1 This is server side error');
+              if (error.status ===0) errorMsg = `TrY To UsE V_P_N`
+              else errorMsg = `1 Error Code FROM HttpInterceptor: ${error.status},  Message: ${error.message}, Status: ${error.status}`;
+              this.showErrorToastr(errorMsg)
+            }
+            console.log(errorMsg);
+            return throwError(errorMsg);
+          })
+        );
+
     } else {
-      return next.handle(request);
+      return next.handle(request)
+        .pipe(
+          catchError((error: HttpErrorResponse) => {
+            let errorMsg = '';
+            if (error.error instanceof ErrorEvent) {
+            console.log('2 This is client side error FROM HttpInterceptor');
+              errorMsg = `Error: ${error.error.message}`;
+            } else {
+            console.log('2 This is server side error');
+              errorMsg = `2 Error Code FROM HttpInterceptor: ${error.status},  Message: ${error.message}`;
+            }
+          console.log(errorMsg);
+            return throwError(errorMsg);
+          })
+        );
     }
   }
+  showErrorToastr(message: string) { // toastr для параллельного отображения сообщений в углу экрана
+    this.toastr.error(message, '(Toastr ERROR from HttpInterceptor)', {
+      timeOut: 4000,
+      positionClass: 'toast-bottom-left',
+    });
+  }
+
 }
